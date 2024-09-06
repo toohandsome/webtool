dbaf357b056b3393a7ee205c670875ce3db5785f
toohandsome <hudcan9@gmail.com>
2024-09-06 18:04:50 +0800

    文本处理

--- components/AceWithMenu.vue
+++ components/AceWithMenu.vue
@@ -40,16 +40,21 @@ export default defineComponent({
   },
   setup() {
     const filterDialogVisible = ref(false);
-    const showFilterDialog = () => {
+    const showFilterDialog1 = () => {
       filterDialogVisible.value = true;
+      filterTitle.value = "筛选/过滤 包含特定文本的行"
+    };
+    const showFilterDialog2 = () => {
+      filterDialogVisible.value = true;
+      filterTitle.value = "筛选/过滤 不包含特定文本的行"
     };
     const menuData = ref([
       {
         "name": "筛选/过滤",
         "event": "",
         "sub": [
-          { "name": "包含特定文本的行", "event": showFilterDialog },
-          { "name": "不包含特定文本的行", "event": showFilterDialog }
+          { "name": "包含特定文本的行", "event": showFilterDialog1 },
+          { "name": "不包含特定文本的行", "event": showFilterDialog2 }
         ]
       },
       {
@@ -162,9 +167,10 @@ export default defineComponent({
     const filterCheckList = ref(["case"]);
     // const searchMode = ref("normal");
     // const searchDirection = ref("down");
-
+    const filterMode = ref("include");
+    const filterTitle = ref("筛选/过滤 包含特定文本的行");
     const easyMode = ref(false);
-    const editerTxt = ref(`console.log('Hello, Ace Editor!');
+    const editerTxt = ref(`console.log('Hello, Ace Editor!');xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/maven-v4_0_0.xsd">xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/maven-v4_0_0.xsd">xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/maven-v4_0_0.xsd">xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/maven-v4_0_0.xsd">xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/maven-v4_0_0.xsd">xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/maven-v4_0_0.xsd">xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/maven-v4_0_0.xsd">xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/maven-v4_0_0.xsd">
 1
 2
 3
@@ -184,11 +190,235 @@ XEX13
 10
 xasx
 searchDialogModald
-sxaspxe
-dashedw
-dasdiew
-ded4
-    
+<?xml version='1.0' encoding='utf-8'?>
+<project xmlns="http://maven.apache.org/POM/4.0.0"
+		 xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+		 xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/maven-v4_0_0.xsd">
+	<modelVersion>4.0.0</modelVersion>
+
+	<packaging>jar</packaging>
+
+	<parent>
+		<groupId>cn.hutool</groupId>
+		<artifactId>hutool-parent</artifactId>
+		<version>5.8.25</version>
+	</parent>
+
+	<artifactId>hutool-core</artifactId>
+	 
+	<description>Hutool核心，包括集合、字符串、Bean等工具</description>
+
+	<properties>
+		<Automatic-Module-Name>cn.hutool.core</Automatic-Module-Name>
+	</properties>
+</project>
+
+   package cn.hutool.core.annotation.scanner;
+
+import cn.hutool.core.annotation.AnnotationUtil;
+import cn.hutool.core.util.ArrayUtil;
+import cn.hutool.core.util.ObjectUtil;
+
+import java.lang.annotation.Annotation;
+import java.lang.annotation.Inherited;
+import java.lang.reflect.AnnotatedElement;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.function.BiConsumer;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * <p>注解扫描器，用于从支持的可注解元素上获取所需注解
+ *
+ * <p>默认提供了以下扫描方式：
+ * <ul>
+ *     <li>{@link #NOTHING}：什么都不做，什么注解都不扫描；</li>
+ *     <li>{@link #DIRECTLY}：扫描元素本身直接声明的注解，包括父类带有{@link Inherited}、被传递到元素上的注解；</li>
+ *     <li>
+ *         {@link #DIRECTLY_AND_META_ANNOTATION}：扫描元素本身直接声明的注解，包括父类带有{@link Inherited}、被传递到元素上的注解，
+ *         以及这些注解的元注解；
+ *     </li>
+ *     <li>{@link #SUPERCLASS}：扫描元素本身以及父类的层级结构中声明的注解；</li>
+ *     <li>{@link #SUPERCLASS_AND_META_ANNOTATION}：扫描元素本身以及父类的层级结构中声明的注解，以及这些注解的元注解；</li>
+ *     <li>{@link #INTERFACE}：扫描元素本身以及父接口的层级结构中声明的注解；</li>
+ *     <li>{@link #INTERFACE_AND_META_ANNOTATION}：扫描元素本身以及父接口的层级结构中声明的注解，以及这些注解的元注解；</li>
+ *     <li>{@link #TYPE_HIERARCHY}：扫描元素本身以及父类、父接口的层级结构中声明的注解；</li>
+ *     <li>{@link #TYPE_HIERARCHY_AND_META_ANNOTATION}：扫描元素本身以及父接口、父接口的层级结构中声明的注解，以及这些注解的元注解；</li>
+ * </ul>
+ *
+ * @author huangchengxing
+ * @see TypeAnnotationScanner
+ * @see MethodAnnotationScanner
+ * @see FieldAnnotationScanner
+ * @see MetaAnnotationScanner
+ * @see ElementAnnotationScanner
+ * @see GenericAnnotationScanner
+ */
+public interface AnnotationScanner {
+
+	// ============================ 预置的扫描器实例 ============================
+
+	/**
+	 * 不扫描任何注解
+	 */
+	AnnotationScanner NOTHING = new EmptyAnnotationScanner();
+
+	/**
+	 * 扫描元素本身直接声明的注解，包括父类带有{@link Inherited}、被传递到元素上的注解的扫描器
+	 */
+	AnnotationScanner DIRECTLY = new GenericAnnotationScanner(false, false, false);
+
+	/**
+	 * 扫描元素本身直接声明的注解，包括父类带有{@link Inherited}、被传递到元素上的注解，以及这些注解的元注解的扫描器
+	 */
+	AnnotationScanner DIRECTLY_AND_META_ANNOTATION = new GenericAnnotationScanner(true, false, false);
+
+	/**
+	 * 扫描元素本身以及父类的层级结构中声明的注解的扫描器
+	 */
+	AnnotationScanner SUPERCLASS = new GenericAnnotationScanner(false, true, false);
+
+	/**
+	 * 扫描元素本身以及父类的层级结构中声明的注解，以及这些注解的元注解的扫描器
+	 */
+	AnnotationScanner SUPERCLASS_AND_META_ANNOTATION = new GenericAnnotationScanner(true, true, false);
+
+	/**
+	 * 扫描元素本身以及父接口的层级结构中声明的注解的扫描器
+	 */
+	AnnotationScanner INTERFACE = new GenericAnnotationScanner(false, false, true);
+
+	/**
+	 * 扫描元素本身以及父接口的层级结构中声明的注解，以及这些注解的元注解的扫描器
+	 */
+	AnnotationScanner INTERFACE_AND_META_ANNOTATION = new GenericAnnotationScanner(true, false, true);
+
+	/**
+	 * 扫描元素本身以及父类、父接口的层级结构中声明的注解的扫描器
+	 */
+	AnnotationScanner TYPE_HIERARCHY = new GenericAnnotationScanner(false, true, true);
+
+	/**
+	 * 扫描元素本身以及父接口、父接口的层级结构中声明的注解，以及这些注解的元注解的扫描器
+	 */
+	AnnotationScanner TYPE_HIERARCHY_AND_META_ANNOTATION = new GenericAnnotationScanner(true, true, true);
+
+	// ============================ 静态方法 ============================
+
+	/**
+	 * 给定一组扫描器，使用第一个支持处理该类型元素的扫描器获取元素上可能存在的注解
+	 *
+	 * @param annotatedEle {@link AnnotatedElement}，可以是Class、Method、Field、Constructor、ReflectPermission
+	 * @param scanners     注解扫描器
+	 * @return 注解
+	 */
+	static List<Annotation> scanByAnySupported(AnnotatedElement annotatedEle, AnnotationScanner... scanners) {
+		if (ObjectUtil.isNull(annotatedEle) && ArrayUtil.isNotEmpty(scanners)) {
+			return Collections.emptyList();
+		}
+		return Stream.of(scanners)
+			.filter(scanner -> scanner.support(annotatedEle))
+			.findFirst()
+			.map(scanner -> scanner.getAnnotations(annotatedEle))
+			.orElseGet(Collections::emptyList);
+	}
+
+	/**
+	 * 根据指定的扫描器，扫描元素上可能存在的注解
+	 *
+	 * @param annotatedEle {@link AnnotatedElement}，可以是Class、Method、Field、Constructor、ReflectPermission
+	 * @param scanners     注解扫描器
+	 * @return 注解
+	 */
+	static List<Annotation> scanByAllSupported(AnnotatedElement annotatedEle, AnnotationScanner... scanners) {
+		if (ObjectUtil.isNull(annotatedEle) && ArrayUtil.isNotEmpty(scanners)) {
+			return Collections.emptyList();
+		}
+		return Stream.of(scanners)
+			.map(scanner -> scanner.getAnnotationsIfSupport(annotatedEle))
+			.flatMap(Collection::stream)
+			.collect(Collectors.toList());
+	}
+
+	// ============================ 抽象方法 ============================
+
+	/**
+	 * 判断是否支持扫描该注解元素
+	 *
+	 * @param annotatedEle {@link AnnotatedElement}，可以是Class、Method、Field、Constructor、ReflectPermission
+	 * @return 是否支持扫描该注解元素
+	 */
+	default boolean support(AnnotatedElement annotatedEle) {
+		return false;
+	}
+
+	/**
+	 * 获取注解元素上的全部注解。调用该方法前，需要确保调用{@link #support(AnnotatedElement)}返回为true
+	 *
+	 * @param annotatedEle {@link AnnotatedElement}，可以是Class、Method、Field、Constructor、ReflectPermission
+	 * @return 注解
+	 */
+	default List<Annotation> getAnnotations(AnnotatedElement annotatedEle) {
+		final List<Annotation> annotations = new ArrayList<>();
+		scan((index, annotation) -> annotations.add(annotation), annotatedEle, null);
+		return annotations;
+	}
+
+	/**
+	 * 若{@link #support(AnnotatedElement)}返回{@code true}，
+	 * 则调用并返回{@link #getAnnotations(AnnotatedElement)}结果，
+	 * 否则返回{@link Collections#emptyList()}
+	 *
+	 * @param annotatedEle {@link AnnotatedElement}，可以是Class、Method、Field、Constructor、ReflectPermission
+	 * @return 注解
+	 */
+	default List<Annotation> getAnnotationsIfSupport(AnnotatedElement annotatedEle) {
+		return support(annotatedEle) ? getAnnotations(annotatedEle) : Collections.emptyList();
+	}
+
+	/**
+	 * 扫描注解元素的层级结构（若存在），然后对获取到的注解和注解对应的层级索引进行处理。
+	 * 调用该方法前，需要确保调用{@link #support(AnnotatedElement)}返回为true
+	 *
+	 * @param consumer     对获取到的注解和注解对应的层级索引的处理
+	 * @param annotatedEle {@link AnnotatedElement}，可以是Class、Method、Field、Constructor、ReflectPermission
+	 * @param filter       注解过滤器，无法通过过滤器的注解不会被处理。该参数允许为空。
+	 */
+	default void scan(BiConsumer<Integer, Annotation> consumer, AnnotatedElement annotatedEle, Predicate<Annotation> filter) {
+		filter = ObjectUtil.defaultIfNull(filter, (a)->annotation -> true);
+		for (final Annotation annotation : annotatedEle.getAnnotations()) {
+			if (AnnotationUtil.isNotJdkMateAnnotation(annotation.annotationType()) && filter.test(annotation)) {
+				consumer.accept(0, annotation);
+			}
+		}
+	}
+
+	/**
+	 * 若{@link #support(AnnotatedElement)}返回{@code true}，则调用{@link #scan(BiConsumer, AnnotatedElement, Predicate)}
+	 *
+	 * @param consumer     对获取到的注解和注解对应的层级索引的处理
+	 * @param annotatedEle {@link AnnotatedElement}，可以是Class、Method、Field、Constructor、ReflectPermission
+	 * @param filter       注解过滤器，无法通过过滤器的注解不会被处理。该参数允许为空。
+	 */
+	default void scanIfSupport(BiConsumer<Integer, Annotation> consumer, AnnotatedElement annotatedEle, Predicate<Annotation> filter) {
+		if (support(annotatedEle)) {
+			scan(consumer, annotatedEle, filter);
+		}
+	}
+
+}
+# 这是一行注释，读取时应忽略
+a,b,c,d
+1,2,3,4
+# 这是一行注释，读取时应忽略
+q,w,e,r,"我是一段
+带换行的内容"
+a,s,d,f
+ 
 <style scoped>
 .wrapper {
   display: flex;
@@ -204,14 +434,30 @@ ded4
     const language = ref("javascript");
     const theme = ref("github");
     const aceEditor = ref(null);
+    //https://blog.csdn.net/qq_32682301/article/details/128871450#:~:text=mode%EF%BC%9A%E7%BC%96%E8%BE%91%E5%99%A8%E7%9A%84%E6%95%B4%E4%BD%93%E6%A8%A1%E5%BC%8F%E6%88%96%E6%A0%B7%E5%BC%8F%EF%BC%8C%E8%BF%99%E9%87%8C%E5%8F%96%E5%80%BC%E4%B8%BA%20markdown%EF%BC%8C%E8%A1%A8%E6%98%8E%E9%9C%80%E8%A6%81%E7%94%A8%E8%BF%99%E4%B8%AA%E7%BC%96%E8%BE%91%E5%99%A8%E6%9D%A5%E8%BE%93%E5%85%A5%20markdown%E6%96%87%E6%9C%AC%EF%BC%8C%E8%BF%99%E6%A0%B7%E7%BC%96%E8%BE%91%E5%99%A8%E5%B0%B1%E4%BC%9A%E8%BF%9B%E8%A1%8C%E7%9B%B8%E5%BA%94%E7%9A%84%E5%88%9D%E5%A7%8B%E8%AE%BE%E7%BD%AE%E3%80%82
     const editorOptions = ref({
       fontSize: "12px",
-      showPrintMargin: false
+      showPrintMargin: false,
+      wrap: true
     });
 
 
 
-    const filterStrFun1 = () => {
+    const filterStrFun = () => {
+
+      if (dialogValue1.value == "") {
+        ElMessage({
+          type: "error",
+          message: `请输入内容`
+        });
+        return;
+      }
+
+      if (filterTitle.value == "筛选/过滤 包含特定文本的行") {
+        filterMode.value = "include";
+      } else {
+        filterMode.value = "not include";
+      }
 
       let reg = new RegExp(dialogValue1.value);
 
@@ -232,13 +478,18 @@ ded4
         if (filterCheckList.value.includes("case")) {
           filterTextarea(filterStrFun2, dialogValue1.value, allText);
         } else {
-          filterTextarea(filterStrFun3, reg, allText);
+          filterTextarea(filterStrFun4, dialogValue1.value, allText);
         }
       }
 
     };
+
+
     const filterStrFun2 = (str1: string, str2: string) => {
       return str1.indexOf(str2) != -1;
+    };
+    const filterStrFun4 = (str1: string, str2: string) => {
+      return str1.toLocaleLowerCase().indexOf(str2.toLocaleLowerCase()) != -1;
     };
     const filterStrFun3 = (str1: string, reg: RegExp) => {
       return reg.test(str1);
@@ -252,11 +503,18 @@ ded4
       let findNum = 0;
       for (let index = 0; index < strArr.length; index++) {
         const element = strArr[index];
-        // console.log("element", element);
-        if (fun(element, condition)) {
-          finalStr += element + "\n";
-          findNum++;
+        if (filterMode.value == "include") {
+          if (fun(element, condition)) {
+            finalStr += element + "\n";
+            findNum++;
+          }
+        } else {
+          if (!fun(element, condition)) {
+            finalStr += element + "\n";
+            findNum++;
+          }
         }
+
       }
       if (findNum > 0) {
         ElMessage({
@@ -279,7 +537,7 @@ ded4
     };
     const searchNext = (searchDirection: string) => {
       console.log("11", searchVal.value);
-      renewSearchOption();
+      // renewSearchOption();
       if (searchDirection == "down") {
         aceEditor.value.findNext();
       } else if (searchDirection == "up") {
@@ -374,6 +632,7 @@ ded4
     };
     const searchOptionChange = () => {
       let searchOption = renewSearchOption();
+      console.log("searchOption",searchOption);
       aceEditor.value.find(searchVal.value, searchOption);
     };
     const replaceText = type => {
@@ -391,6 +650,7 @@ ded4
     };
 
     watch(searchVal, (New, Old) => {
+      console.log("new: ",New);
       let searchOption = renewSearchOption();
       aceEditor.value.find(searchVal.value, searchOption);
     });
@@ -415,8 +675,8 @@ ded4
       dialogVisible,
       dialogValue1,
       dialogValue2,
-      filterStrFun1,
-      filterStrFun2,
+      filterStrFun,
+
       filterTextarea,
       searchDialogVisible,
       searchDialogModal,
@@ -444,7 +704,7 @@ ded4
       filterCheckList,
       easyMode,
       menuData,
-      showFilterDialog
+      filterTitle
     };
   }
 });
@@ -531,7 +791,7 @@ ded4
     <!-- 右键菜单过滤弹窗-->
     <el-dialog v-model="filterDialogVisible" :modal="searchDialogModal" :close-on-click-modal="false"
       modal-class="my-dialog-class" width="500" draggable overflow :style="{ opacity: searchDialogOpacity }"
-      title="筛选/过滤条件" @focusin="searchDialogFocus('open')" @focusout="searchDialogFocus('close')"
+      :title="filterTitle" @focusin="searchDialogFocus('open')" @focusout="searchDialogFocus('close')"
       style="pointer-events: auto;">
       <div style="display: flex">
         <el-text>内容 ：</el-text><el-input v-model="dialogValue1" style="width: 350px" clearable autofocus />
@@ -544,13 +804,13 @@ ded4
           <el-checkbox disabled label="新编辑器" value="newEditor" />
 
         </el-checkbox-group>
-        <div>
+        <!-- <div>
           <el-text>结果 ：</el-text>
           <el-radio-group>
             <el-radio value="copy">复制到剪贴板</el-radio>
             <el-radio label="cut">剪切到剪贴板</el-radio>
           </el-radio-group>
-        </div>
+        </div> -->
       </div>
       <div />
       <template #footer>
@@ -558,7 +818,7 @@ ded4
           <el-button @click="filterDialogVisible = false">取消</el-button>
           <el-button type="primary" @click="filterDialogVisible = false">预览</el-button>
 
-          <el-button type="danger" @click="filterStrFun1"> 确定 </el-button>
+          <el-button type="danger" @click="filterStrFun()"> 确定 </el-button>
         </div>
       </template>
     </el-dialog>
