2e5a0c4809e7bfb204398d810b6f86dcb27b2aa2
toohandsome <46422252+toohandsome@users.noreply.github.com>
2024-09-07 14:40:46 +0000

    集成MonacoEditor

---
+++ pages/MyMonaco.vue
@@ -0,0 +1,21 @@
+<template>
+  <section class="monaco-editor-container">
+    <MonacoEditor
+      :old-value="sourceValue"
+      :new-value="targetValue"
+      @original-change="originalChange"
+    />
+  </section>
+</template>
+
+<script setup lang="ts">
+ import { ref, defineComponent } from "vue";
+ 
+const sourceValue = ref('123')
+const targetValue = ref('123aaaaa')
+
+const originalChange = (val: string) => {
+    sourceValue.value = val
+}
+</script>
+ 
